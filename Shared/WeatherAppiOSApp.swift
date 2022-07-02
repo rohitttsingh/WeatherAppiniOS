e6ef81aee69f09264e745d849096c079cd7bc95a
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-07-02 23:33:05 +0530

    v 1.1.1

---
+++ Shared/WeatherAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppiOSApp.swift
+//  Shared
+//
+//  Created by Rohit Singh on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
